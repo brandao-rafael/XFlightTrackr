b547b74a476531e7914f88ebcaddc30f32196285
Rafael <rafaelbrandao1992@gmail.com>
2023-07-12 18:23:53 -0300

    fix: build gradle

--- x_flight_trackr/lib/pages/trackr_map_page.dart
+++ x_flight_trackr/lib/pages/trackr_map_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:x_flight_trackr/utils/location_util.dart';
 
 class TrackrMapPage extends StatelessWidget {
   const TrackrMapPage({super.key});
@@ -11,10 +10,15 @@ class TrackrMapPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Trackr Map'),
         ),
-        body: const GoogleMap(
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-22.42556, -45.45278),
-          ),
-        ));
+        body: GoogleMap(
+            initialCameraPosition: const CameraPosition(
+              target: LatLng(-22.42556, -45.45278),
+            ),
+            markers: {
+              const Marker(
+                markerId: MarkerId('1'),
+                position: LatLng(-22.42556, -45.45278),
+              ),
+            }));
   }
 }
