1f0f5a1461ef8baa904e0c0409780d47d03446ce
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-01 15:25:00 -0300

    refactor: to follow best pratices

--- x_flight_trackr/lib/pages/home_page.dart
+++ x_flight_trackr/lib/pages/home_page.dart
@@ -6,21 +6,21 @@ import 'package:x_flight_trackr/utils/udp_utils.dart';
 import 'package:x_flight_trackr/utils/xplane_data_parser.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  List<double> allData = [];
+  List<double> _allData = [];
 
   void _init(Datagram datagram) {
     var xpc = XPlaneDataParser(datagram.data);
     List<double> parsedData;
     parsedData = xpc.parseDATA();
     setState(() {
-      allData = parsedData;
+      _allData = parsedData;
     });
   }
 
@@ -29,42 +29,49 @@ class _HomePageState extends State<HomePage> {
     return MaterialApp(
       title: 'XFlightTrackr',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.blueAccent[700]),
-          ),
-        ),
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.lightBlue,
-          foregroundColor: Colors.white,
-        ),
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(color: Colors.white),
-          bodyMedium: TextStyle(color: Colors.white),
-          bodySmall: TextStyle(color: Colors.white),
-        ),
-      ),
+      theme: _buildThemeData(),
       home: FutureBuilder<Stream<Datagram>>(
         future: UdpUtils.bindUdp(),
-        builder: (ctx, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          snapshot.data!.listen(_init);
-          return TrackrMapPage(
-            lat: allData.isNotEmpty ? allData[171] : 0.0,
-            lng: allData.isNotEmpty ? allData[172] : 0.0,
-            mag: allData.isNotEmpty ? allData[147] : 0.0,
-            data: allData,
-          );
-        },
+        builder: _buildHomePage,
       ),
+    );
+  }
+
+  ThemeData _buildThemeData() {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent[700]!),
+      useMaterial3: true,
+      textButtonTheme: TextButtonThemeData(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(Colors.blueAccent[700]),
+        ),
+      ),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.blueAccent[700] ?? Colors.blueAccent,
+        foregroundColor: Colors.white,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Colors.white),
+        bodyMedium: TextStyle(color: Colors.white),
+        bodySmall: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+
+  Widget _buildHomePage(
+      BuildContext context, AsyncSnapshot<Stream<Datagram>> snapshot) {
+    if (snapshot.hasError) {
+      return Center(child: Text('Error: ${snapshot.error}'));
+    }
+    if (!snapshot.hasData) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    snapshot.data!.listen(_init);
+    return TrackrMapPage(
+      lat: _allData.isNotEmpty ? _allData[171] : 0.0,
+      lng: _allData.isNotEmpty ? _allData[172] : 0.0,
+      mag: _allData.isNotEmpty ? _allData[147] : 0.0,
+      data: _allData,
     );
   }
 }
--- x_flight_trackr/lib/pages/trackr_map_page.dart
+++ x_flight_trackr/lib/pages/trackr_map_page.dart
@@ -1,7 +1,6 @@
 import 'dart:typed_data';
-import 'package:flutter/services.dart' show rootBundle;
 import 'dart:ui' as ui;
-
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:x_flight_trackr/components/flight_data.dart';
@@ -14,44 +13,44 @@ class TrackrMapPage extends StatefulWidget {
   final List<double> data;
 
   const TrackrMapPage({
-    super.key,
+    Key? key,
     required this.lat,
     required this.lng,
     required this.mag,
     required this.data,
-  });
+  }) : super(key: key);
 
   @override
   State<TrackrMapPage> createState() => _TrackrMapPageState();
 }
 
 class _TrackrMapPageState extends State<TrackrMapPage> {
-  Uint8List? customMarker;
+  Uint8List? _customMarker;
   MapType _currentMapType = MapType.terrain;
-
-  Future<Uint8List?> getBytesFromAsset() async {
-    ByteData data = await rootBundle.load('lib/assets/icons/airplane_icon.png');
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
-        targetWidth: 128);
-    ui.FrameInfo fi = await codec.getNextFrame();
-    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
-        ?.buffer
-        .asUint8List();
-  }
-
-  void setMapType(MapType type) {
-    setState(() {
-      _currentMapType = type;
-    });
-  }
 
   @override
   void initState() {
     super.initState();
-    getBytesFromAsset().then((value) {
-      setState(() {
-        customMarker = value;
-      });
+    _loadCustomMarker();
+  }
+
+  Future<void> _loadCustomMarker() async {
+    ByteData data = await rootBundle.load('lib/assets/icons/airplane_icon.png');
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+        targetWidth: 128);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    Uint8List? markerBytes =
+        (await fi.image.toByteData(format: ui.ImageByteFormat.png))
+            ?.buffer
+            .asUint8List();
+    setState(() {
+      _customMarker = markerBytes;
+    });
+  }
+
+  void _setMapType(MapType type) {
+    setState(() {
+      _currentMapType = type;
     });
   }
 
@@ -75,27 +74,33 @@ class _TrackrMapPageState extends State<TrackrMapPage> {
                 markerId: const MarkerId('1'),
                 position: LatLng(widget.lat, widget.lng),
                 rotation: widget.mag - 90,
-                icon: customMarker != null && customMarker!.isNotEmpty
-                    ? BitmapDescriptor.fromBytes(customMarker!)
+                icon: _customMarker != null && _customMarker!.isNotEmpty
+                    ? BitmapDescriptor.fromBytes(_customMarker!)
                     : BitmapDescriptor.defaultMarker,
               ),
             },
           ),
-          SelectMapType(setMapType: setMapType),
-          if (widget.data.isNotEmpty)
-            FlightData(data: widget.data)
-          else
-            Positioned(
-              bottom: 0,
-              child: Container(
-                  color: const Color.fromRGBO(255, 255, 255, 0.498),
-                  padding: const EdgeInsets.all(8.0),
-                  height: 100,
-                  width: MediaQuery.of(context).size.width,
-                  child: const Text('No data available')),
-            )
+          SelectMapType(setMapType: _setMapType),
+          _buildFlightDataOrEmptyState(),
         ],
       ),
     );
+  }
+
+  Widget _buildFlightDataOrEmptyState() {
+    return widget.data.isNotEmpty
+        ? FlightData(data: widget.data)
+        : Positioned(
+            bottom: 0,
+            child: Container(
+              alignment: AlignmentDirectional.topCenter,
+              color: const Color.fromARGB(125, 8, 8, 8),
+              padding: const EdgeInsets.all(8.0),
+              height: 100,
+              width: MediaQuery.of(context).size.width,
+              child: const Text(
+                  'No data available, please verify your windows defender rules or upd port in Xplane, the port need to be 51000'),
+            ),
+          );
   }
 }
