c5ce990f97492608199235e406569c57cdae3125
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-08 15:40:33 -0300

    refactor: to follow best pratices

--- x_flight_trackr/lib/pages/trackr_map_page.dart
+++ x_flight_trackr/lib/pages/trackr_map_page.dart
@@ -1,16 +1,18 @@
 import 'dart:io';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
-import 'package:flutter/services.dart' show rootBundle;
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 import 'package:provider/provider.dart';
+
 import 'package:x_flight_trackr/components/flight_data.dart';
 import 'package:x_flight_trackr/components/flight_plan_details.dart';
 import 'package:x_flight_trackr/components/search_flight_plan.dart';
 import 'package:x_flight_trackr/components/select_map_type.dart';
 import 'package:x_flight_trackr/providers/flight_plan_provider.dart';
-import 'package:google_polyline_algorithm/google_polyline_algorithm.dart';
 
 class TrackrMapPage extends StatefulWidget {
   final double lat;
@@ -27,47 +29,41 @@ class TrackrMapPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<TrackrMapPage> createState() => _TrackrMapPageState();
+  _TrackrMapPageState createState() => _TrackrMapPageState();
 }
 
 class _TrackrMapPageState extends State<TrackrMapPage> {
   Uint8List? _customMarker;
   MapType _currentMapType = MapType.terrain;
-  String ip = 'Searching for ip...';
+  String _ip = 'Searching for ip...';
 
   @override
   void initState() {
     super.initState();
-    getBytesFromAsset().then((value) {
-      setState(() {
-        _customMarker = value;
-      });
+    _loadCustomMarker();
+  }
+
+  Future<void> _loadCustomMarker() async {
+    final value = await getBytesFromAsset();
+    setState(() {
+      _customMarker = value;
     });
   }
 
-  List<LatLng> convertPoints(List<List<num>> points) {
-    List<LatLng> latLngList = [];
-    for (var point in points) {
-      double lat = point[0].toDouble();
-      double lng = point[1].toDouble();
-      latLngList.add(LatLng(lat, lng));
-    }
-    return latLngList;
-  }
+  List<LatLng> convertPoints(List<List<num>> points) => points
+      .map((point) => LatLng(point[0].toDouble(), point[1].toDouble()))
+      .toList();
 
   List<LatLng> decodePolylines(String polyline) {
-    List<LatLng> latLngList = [];
     final polylinePoints = decodePolyline(polyline);
-    latLngList = convertPoints(polylinePoints);
-
-    return latLngList;
+    return convertPoints(polylinePoints);
   }
 
   Future<Uint8List?> getBytesFromAsset() async {
-    ByteData data = await rootBundle.load('lib/assets/icons/airplane_icon.png');
-    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
+    final data = await rootBundle.load('lib/assets/icons/airplane_icon.png');
+    final codec = await ui.instantiateImageCodec(data.buffer.asUint8List(),
         targetWidth: 128);
-    ui.FrameInfo fi = await codec.getNextFrame();
+    final fi = await codec.getNextFrame();
     return (await fi.image.toByteData(format: ui.ImageByteFormat.png))
         ?.buffer
         .asUint8List();
@@ -81,91 +77,118 @@ class _TrackrMapPageState extends State<TrackrMapPage> {
 
   @override
   Widget build(BuildContext context) {
-    final FlightPlanProvider flightPlanProvider =
+    final flightPlanProvider =
         Provider.of<FlightPlanProvider>(context, listen: true);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('XFlight Trackr'),
-        centerTitle: true,
-      ),
+      appBar: _buildAppBar(),
       body: Stack(
         children: [
-          GoogleMap(
-            mapType: _currentMapType,
-            zoomControlsEnabled: false,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(widget.lat, widget.lng),
-            ),
-            markers: {
-              Marker(
-                markerId: const MarkerId('1'),
-                position: LatLng(widget.lat, widget.lng),
-                rotation: widget.mag - 90,
-                icon: _customMarker != null && _customMarker!.isNotEmpty
-                    ? BitmapDescriptor.fromBytes(_customMarker!)
-                    : BitmapDescriptor.defaultMarker,
-              ),
-            },
-            polylines:
-                flightPlanProvider.selectedFlightPlan['encodedPolyline'] != null
-                    ? <Polyline>{
-                        Polyline(
-                          polylineId: const PolylineId('1'),
-                          color: Colors.red,
-                          width: 3,
-                          points: decodePolylines(flightPlanProvider
-                              .selectedFlightPlan['encodedPolyline']),
-                          geodesic: true,
-                          jointType: JointType.round,
-                        ),
-                      }
-                    : <Polyline>{},
-          ),
-          Column(
-            children: [
-              SelectMapType(setMapType: _setMapType),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                const SearchFlightPlan(),
-                FlightPlanDetails(
-                    selectedFlightPlan: flightPlanProvider.selectedFlightPlan),
-              ])
-            ],
-          ),
+          _buildGoogleMap(flightPlanProvider),
+          _buildOverlays(flightPlanProvider),
           _buildFlightDataOrEmptyState(),
         ],
       ),
     );
   }
 
-  Widget _buildFlightDataOrEmptyState() {
-    Future<void> viewIps() async {
-      for (var interface in await NetworkInterface.list()) {
-        if (interface.name.contains('wlan')) {
-          setState(() {
-            ip = '${interface.addresses.map((addr) => addr.address)}';
-          });
-        } else {
-          setState(() {
-            ip = 'Ip not found';
-          });
-        }
-      }
-    }
+  AppBar _buildAppBar() {
+    return AppBar(
+      title: const Text('XFlight Trackr'),
+      centerTitle: true,
+    );
+  }
 
-    viewIps();
+  GoogleMap _buildGoogleMap(FlightPlanProvider flightPlanProvider) {
+    return GoogleMap(
+      mapType: _currentMapType,
+      zoomControlsEnabled: false,
+      initialCameraPosition: CameraPosition(
+        target: LatLng(widget.lat, widget.lng),
+      ),
+      markers: _buildMarkers(),
+      polylines: _buildPolylines(flightPlanProvider),
+    );
+  }
+
+  Set<Marker> _buildMarkers() {
+    return {
+      Marker(
+        markerId: const MarkerId('1'),
+        position: LatLng(widget.lat, widget.lng),
+        rotation: widget.mag - 90,
+        icon: _customMarker != null && _customMarker!.isNotEmpty
+            ? BitmapDescriptor.fromBytes(_customMarker!)
+            : BitmapDescriptor.defaultMarker,
+      ),
+    };
+  }
+
+  Set<Polyline> _buildPolylines(FlightPlanProvider flightPlanProvider) {
+    if (flightPlanProvider.selectedFlightPlan['encodedPolyline'] != null) {
+      return {
+        Polyline(
+          polylineId: const PolylineId('1'),
+          color: Colors.red,
+          width: 3,
+          points: decodePolylines(
+              flightPlanProvider.selectedFlightPlan['encodedPolyline']),
+          geodesic: true,
+          jointType: JointType.round,
+        ),
+      };
+    }
+    return {};
+  }
+
+  Column _buildOverlays(FlightPlanProvider flightPlanProvider) {
+    return Column(
+      children: [
+        SelectMapType(setMapType: _setMapType),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const SearchFlightPlan(),
+            FlightPlanDetails(
+                selectedFlightPlan: flightPlanProvider.selectedFlightPlan),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFlightDataOrEmptyState() {
+    _viewIps();
 
     return widget.data.isNotEmpty
         ? FlightData(data: widget.data)
         : Positioned(
             bottom: 0,
-            child: Container(
-              color: const ui.Color.fromARGB(125, 7, 7, 7),
-              padding: const EdgeInsets.all(8.0),
-              height: 150,
-              width: MediaQuery.of(context).size.width,
-              child: Text(
-                  "Data is currently unavailable. If you are in flight, please verify the Windows Defender rules for X-Plane and confirm that the correct ip and port is being used. Your mobile ip is $ip Within X-Plane, the data output should be set to UDP and the port should be configured to 51000."),
-            ),
+            child: _buildEmptyStateContainer(),
           );
+  }
+
+  Future<void> _viewIps() async {
+    for (var interface in await NetworkInterface.list()) {
+      if (interface.name.contains('wlan')) {
+        setState(() {
+          _ip = '${interface.addresses.map((addr) => addr.address)}';
+        });
+      } else {
+        setState(() {
+          _ip = 'Ip not found';
+        });
+      }
+    }
+  }
+
+  Container _buildEmptyStateContainer() {
+    return Container(
+      color: const ui.Color.fromARGB(125, 7, 7, 7),
+      padding: const EdgeInsets.all(8.0),
+      height: 150,
+      width: MediaQuery.of(context).size.width,
+      child: Text(
+          "Data is currently unavailable. If you are in flight, please verify the Windows Defender rules for X-Plane and confirm that the correct ip and port is being used. Your mobile ip is $_ip. Within X-Plane, the data output should be set to UDP and the port should be configured to 51000."),
+    );
   }
 }
