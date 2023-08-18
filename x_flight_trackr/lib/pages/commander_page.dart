ceb541aadb763c72ac1ace9f54869a0627e45517
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-18 14:43:23 -0300

    chore: create autopilot store

---
+++ x_flight_trackr/lib/pages/commander_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommanderPage extends StatelessWidget {
+  const CommanderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ x_flight_trackr/lib/store/autopilot_store.dart
@@ -0,0 +1,130 @@
+import 'package:mobx/mobx.dart';
+import 'package:x_flight_trackr/utils/commanders/autopilot_commander.dart';
+import 'package:x_flight_trackr/utils/commanders/x_plane_commander.dart';
+
+part 'autopilot_store.g.dart';
+
+class AutopilotStore = _AutopilotStore with _$AutopilotStore;
+
+abstract class _AutopilotStore with Store {
+  XPlaneCommander command =
+      XPlaneCommander(xplaneAddress: '192.168.50.157', xplanePort: 49000);
+
+  late AutoPilotCommander autopilotCommander;
+
+  _AutopilotStore() {
+    autopilotCommander = AutoPilotCommander(commander: command);
+  }
+  @observable
+  AutoPilotMode mode = AutoPilotMode.OFF;
+
+  @observable
+  AutoPilotBankAngle bankAngle = AutoPilotBankAngle.AUTO;
+
+  @observable
+  AutoPilotAltitudeMode altitudeMode = AutoPilotAltitudeMode.PITCH;
+
+  @observable
+  AutoPilotHeadingMode headingMode = AutoPilotHeadingMode.ROLL;
+
+  @observable
+  double altitude = 0;
+  @observable
+  double heading = 0;
+  @observable
+  double verticalSpeed = 0;
+  @observable
+  double airspeed = 0;
+  @observable
+  bool autoThrottle = false;
+
+  @action
+  Future<void> setMode(AutoPilotMode newMode) async {
+    try {
+      mode = newMode;
+      await autopilotCommander.setAutopilotMode(mode);
+    } catch (_) {
+      mode = AutoPilotMode.OFF;
+    }
+  }
+
+  @action
+  Future<void> setBankAngle(AutoPilotBankAngle newBankAngle) async {
+    try {
+      bankAngle = newBankAngle;
+      await autopilotCommander.setAutoPilotBankAngle(bankAngle);
+    } catch (_) {
+      bankAngle = AutoPilotBankAngle.AUTO;
+    }
+  }
+
+  @action
+  Future<void> setAltitudeMode(AutoPilotAltitudeMode newAltitudeMode) async {
+    try {
+      altitudeMode = newAltitudeMode;
+      await autopilotCommander.setAutopilotAltitudeMode(altitudeMode);
+    } catch (_) {
+      altitudeMode = AutoPilotAltitudeMode.PITCH;
+    }
+  }
+
+  @action
+  Future<void> setHeadingMode(AutoPilotHeadingMode newHeadingMode) async {
+    try {
+      headingMode = newHeadingMode;
+      await autopilotCommander.setAutopilotHeadingMode(headingMode);
+    } catch (_) {
+      headingMode = AutoPilotHeadingMode.ROLL;
+    }
+  }
+
+  @action
+  Future<void> setAltitude(double newAltitude) async {
+    try {
+      altitude = newAltitude;
+      await autopilotCommander.setAutopilotAltitude(altitude);
+    } catch (_) {
+      altitude = 0;
+    }
+  }
+
+  @action
+  Future<void> setHeading(double newHeading) async {
+    try {
+      heading = newHeading;
+      await autopilotCommander.setAutopilotHeading(heading);
+    } catch (_) {
+      heading = 0;
+    }
+  }
+
+  @action
+  Future<void> setVerticalSpeed(double newVerticalSpeed) async {
+    try {
+      verticalSpeed = newVerticalSpeed;
+      await autopilotCommander.setAutopilotVerticalSpeed(verticalSpeed);
+    } catch (_) {
+      verticalSpeed = 0;
+    }
+  }
+
+  @action
+  Future<void> setAirspeed(double newAirspeed) async {
+    try {
+      airspeed = newAirspeed;
+      await autopilotCommander.setAutopilotAirspeed(airspeed);
+    } catch (_) {
+      airspeed = 0;
+    }
+  }
+
+  @action
+  Future<void> setAutoThrottle(bool newAutoThrottle) async {
+    try {
+      autoThrottle = newAutoThrottle;
+      await autopilotCommander.setAutoThrottleEnabled(newAutoThrottle);
+    } catch (e) {
+      autoThrottle = false;
+    }
+  }
+}
