47291fdd3fc490405f818d0222de7031bfaf3ff9
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-23 17:15:24 -0300

    refactor: fix some bugs

--- x_flight_trackr/lib/components/flight_plan_header.dart
+++ x_flight_trackr/lib/components/flight_plan_header.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:x_flight_trackr/components/flight_plan_database_logo.dart';
 
 class FlightPlanHeader extends StatelessWidget {
   const FlightPlanHeader({Key? key}) : super(key: key);
-
-  Future<void> _launchUrl(String url) async {
-    if (!await launchUrl(Uri.parse(url))) {
-      null;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +13,7 @@ class FlightPlanHeader extends StatelessWidget {
         Text(
           'Search Flight Plan',
           style: TextStyle(
-            fontSize: 25,
+            fontSize: 22,
             color: Colors.blueAccent[700]!,
             fontWeight: FontWeight.w300,
           ),
